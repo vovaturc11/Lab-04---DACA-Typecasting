030e099f43f515b2abdee4c23b9a57011b1f296d
LAB4804WD29 <w7415979@student.miracosta.edu>
2026-06-18 09:33:17 -0700

    Finished the project

--- Main.java
+++ Main.java
@@ -1,22 +1,33 @@
-// YOUR NAME: 
+// YOUR NAME: Volodymyr Turchanskyi
 // COLLABORATORS:  
-// DATE: 
+// DATE: 06/18/2026
 
 public class Main
 {
 	public static void main(String[] args)
 	{
 		// DECLARATION SECTION
-
+		int dacaRecipients;
+		long totalFederalTaxes;
+		double averageDacaContribution;
+		int roundedDownTaxContribution;
+		String stateName;
 		// INITIALIZATION SECTION
-		
+		char characterFirst = 'c';
+		char characterSecond = 'a';
+		dacaRecipients = 700000;
+		totalFederalTaxes = 60000000000L;
 		// INPUT SECTION
 		// N/A (no input for this lab)
 		
 		// CALCULATION & PROCESSING SECTION
-
-		
+		averageDacaContribution = (double) totalFederalTaxes/dacaRecipients;
+		roundedDownTaxContribution = (int) averageDacaContribution;
+		stateName = "" + (char) (characterFirst - 32) + (char) (characterSecond - 32);
 		// OUTPUT SECTION
-		
+		System.out.println("Average DACA-recipient Tax Contribution: " + averageDacaContribution);
+		System.out.println("Rounded down to nearest whole dollar: $" + roundedDownTaxContribution);
+		System.out.println("State with most DACA recipients: " + stateName);
+
 	}
 }
